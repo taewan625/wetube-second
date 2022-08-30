bb87a05b28c94bfeccffaf15b825c89ee2def91d
taewan625 <taewan1997625@gmail.com>
2022-08-30 09:19:06 +0900

    ~#3.2

--- src/server.js
+++ src/server.js
@@ -1,18 +1,20 @@
-// const express = require("express"); old code
-import express from "express"; // node module에서 express 찾음
+import express from "express";
 
 const PORT = 4000;
 
-const app = express(); //creates express application
+// 1) create server
+const app = express(); // relate express code have to wirte down this line
 
-// what is server? app=server have to wait for listenomg request  / server is computer that always running and connect with internet.
-// request meaning is go to other URL
+// 2) configue(설정) server
+const handleHome = () => console.log("hi server");
+app.get("/", handleHome); // browser의 get request "/". server의 respond(handleHome)
 
+// 3) open server
 const handleListening = () =>
   console.log(`✅ server listening on port http://localhost:${PORT}`);
 
-// server need to wait client request => waiting is callback;when server start, that time running
-// before connect server, say to server what port we listen.  port; window
 app.listen(PORT, handleListening);
 
-// server에 접속하는 방법: 보통은 localhost를 통해서 접속이 가능 -> when demon die server also die
+// get is HTTP method, http is communicate with server method
+// 1) how http reuest working ; app.get의 get이 browser가 server에게 request 보내는 것 *request: browser said hey I want to get URL
+// 2) how to respond GET_request ; request ex) "/" 들어오면 server은 그 request를 다룰 수 있다 ex) handleHome

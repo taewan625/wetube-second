db435cbd342262acd34b0b12c94b82a0cc264942
taewan625 <taewan1997625@gmail.com>
2022-09-02 13:00:52 +0900

    #3.10

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const PORT = 8000;
 const app = express();
 
 // 2) configue(설정) server
-const gossipMiddleware = (req, res, next) => {
+const logger = (req, res, next) => {
   console.log("I'm in the middle!");
   // return res.send("is it work????"); // cant go final controller
   next();
@@ -14,10 +14,11 @@ const gossipMiddleware = (req, res, next) => {
 const handleHome = (req, res, next) => {
   // last res func이라서 사실상 next가 필요 없음
   return res.end();
-  return res.send("res.send()");
 };
-
-app.get("/", gossipMiddleware, handleHome);
+// const handleHome = (req, res, next) => res.end();
+// express는 이런 화살표 함수일 때 return을 함유하고 있어서 따로 안써줘도 됨 중요!!!
+// 그렇다고 { }으로 코드 쓸 때 무조건 return을 써야된다는 것은 아니다!!
+app.get("/", logger, handleHome);
 // express work handleHome({...req...}, {...res...})
 
 // 3) open server

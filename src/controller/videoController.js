b98b2a78cd8b84f4f35f1c427ccf03fef8d29d85
taewan625 <taewan1997625@gmail.com>
2022-09-02 21:05:12 +0900

    ~4.8

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -1,5 +1,8 @@
 export const trending = (req, res) => res.send("Home page");
-export const see = (req, res) => res.send("see");
+export const see = (req, res) => {
+  console.log(req.params); // :id 이므로 id: "params" 로 나옴
+  return res.send(`watch video ${req.params.id}`);
+};
 export const edit = (req, res) => res.send("edit");
 export const search = (req, res) => res.send("search");
 export const upload = (req, res) => res.send("upload");
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -4,8 +4,13 @@ import { upload, see, edit, deleteVideo } from "../controller/videoController";
 const videoRouter = express.Router();
 
 videoRouter.get("/upload", upload);
-videoRouter.get("/:id", see);
-videoRouter.get("/:id/edit", edit);
-videoRouter.get("/:id/delete", deleteVideo);
+// express가 똑똑해서 라우터 url에 :가 들어간것을 확인하면 이게 변수라는 것을 인지함
+// 그래서 url에 videos/123123sagag!@#@$%#해도 정상 작동을 한다.
+// 그리고 console.log로 req.params를 확인해보면 :뒤에 있는 variable로 parameter값을 보여준다.
+
+// parameter에서 \d+로도 테스트가 됬는데 JS에서 적용할때는 \\써야함
+videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id(\\d+)/edit", edit);
+videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 
 export default videoRouter;

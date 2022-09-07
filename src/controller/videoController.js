f06ee6dc2d17337a4ecddae95f32f8079e827362
taewan625 <taewan1997625@gmail.com>
2022-09-07 17:04:26 +0900

    -6.6

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -1,3 +1,5 @@
+import { request } from "express";
+
 let videos = [
   { title: "video1", rating: 5, views: 0, id: 1 },
   { title: "video2", rating: 5, views: 1, id: 2 },
@@ -13,7 +15,7 @@ export const watch = (req, res) => {
 };
 export const getEdit = (req, res) => {
   const { id } = req.params; // 주소에 포함된 변수를 담는다. 예를 들어 https://localhost:8000/videos/12345/edit 라는 주소가 있다면 12345를 담는다
-  const video = videos[id - 0];
+  const video = videos[id - 1];
   return res.render("edit", { pageTitle: `${video.title} edit`, video });
 };
 export const postEdit = (req, res) => {
@@ -22,4 +24,15 @@ export const postEdit = (req, res) => {
   console.log(req.body);
   videos[id - 1].title = title; // fakedatabase라서 이럴수 밖에없음
   return res.redirect(`/videos/${id}`);
+};
+
+export const getUpload = (req, res) => {
+  return res.render("upload", { pageTitle: "upload video" });
+};
+
+export const postUpload = (req, res) => {
+  const { title } = req.body;
+  const newVideo = { title, rating: 6, views: 0, id: videos.length + 1 };
+  videos.push(newVideo);
+  return res.redirect("/");
 };
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,10 +1,17 @@
 import express from "express";
-import { watch, getEdit, postEdit } from "../controller/videoController";
+import {
+  watch,
+  getEdit,
+  postEdit,
+  getUpload,
+  postUpload,
+} from "../controller/videoController";
 
 const videoRouter = express.Router();
 
 // parameter에서 \d+로도 테스트가 됬는데 JS에서 적용할때는 \\써야함 escape라고 하고 \\해야 \개로 출력됨
 videoRouter.get("/:id(\\d+)", watch);
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;

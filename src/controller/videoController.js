282d05bb656a4370ccdc48e138f80a089fc8772a
taewan625 <taewan1997625@gmail.com>
2022-09-07 12:22:39 +0900

    6.0

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -1,12 +1,16 @@
+let videos = [
+  { title: "video1", rating: 5, views: 39, id: 1 },
+  { title: "video2", rating: 5, views: 39, id: 2 },
+  { title: "video3", rating: 5, views: 39, id: 3 },
+];
 export const trending = (req, res) => {
-  const videos = [
-    { title: "video1", rating: 5, views: 39, id: 1 },
-    { title: "video2", rating: 5, views: 39, id: 2 },
-    { title: "video3", rating: 5, views: 39, id: 3 },
-  ];
   return res.render("home", { pageTitle: "Home", videos });
 };
-export const see = (req, res) => res.render("watch");
+export const see = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: video.title });
+};
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("search");
 export const upload = (req, res) => res.send("upload");

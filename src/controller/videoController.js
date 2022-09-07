8bcdfdcea4f5272c53bb64cf024748fb8a7c0d30
taewan625 <taewan1997625@gmail.com>
2022-09-07 10:14:52 +0900

    -5.7

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -1,4 +1,10 @@
-export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+const fakeUser = {
+  username: "wooweee",
+  loggedIn: false,
+};
+
+export const trending = (req, res) =>
+  res.render("home", { pageTitle: "Home", fakeUser }); // home에서 fakeUser data를 읽기위해서 variable에 넣음!!
 export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("search");

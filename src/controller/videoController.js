432c654761e7f7978dda4d72bc7309e10f53d83c
taewan625 <taewan1997625@gmail.com>
2022-09-12 15:06:20 +0900

    6-15

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -1,12 +1,8 @@
 import VideoModel from "../models/Video";
 
 export const home = async (req, res) => {
-  try {
-    const videos = await VideoModel.find({});
-    return res.render("home", { pageTitle: "Home", videos });
-  } catch (error) {
-    return res.render("server-error", { error });
-  }
+  const videos = await VideoModel.find({});
+  return res.render("home", { pageTitle: "Home", videos });
 };
 export const watch = (req, res) => {
   const { id } = req.params;
@@ -26,7 +22,21 @@ export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "upload video" });
 };
 
+/* upload video의 input data save 후 db에 저장하는 방법
+1) document 만들기
+2) database에 저장
+*/
+// 1) video doc은 만들어졌지만 아직 JS에서만 존재
 export const postUpload = (req, res) => {
-  const { title } = req.body;
+  const { title, description, hashtags } = req.body;
+  const video = new VideoModel({
+    title,
+    description,
+    createdAt: Date.now(),
+    hashtags: hashtags.split(",").map((word) => `#${word}`),
+    meta: { views: 0, rating: 0 },
+    // mongoose가 고유 _id도 준다.
+  });
+  console.log(video);
   return res.redirect("/");
 };

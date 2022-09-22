b38bf579bcf3c8f9dbcf0e08099f3ebc6743ba39
taewan625 <taewan1997625@gmail.com>
2022-09-22 20:29:12 +0900

    9.5 -css js 파일 분리

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,19 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   // webpack compile-file이 empty file로 나오는 경우
   mode: "development",
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/styles.css",
+    }),
+  ],
   // entry -> webpack.config.js -> output || to change old code for every browser
   entry: "./src/client/js/main.js",
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "assets", "js"),
+    filename: "js/main.js",
+    path: path.resolve(__dirname, "assets"),
   },
   // some kind of latest code need 호환성. for that kind of problem, we use [css or js ]-loader
   // 1) js, babel-loader
@@ -26,7 +32,8 @@ module.exports = {
         test: /\.scss$/,
         // 여러가지 loader combine 할거면 array 사용. 1) scss -> css 2) css -> browser 3) beautiful font
         // 빡치는게 webpack은 오른쪽부터 읽어나간다.
-        use: ["style-loader", "css-loader", "sass-loader"],
+        // style-loader 대신에 mini.loader로 변경 -> css file을 main.js 밖으로 분리해서 만들기 위함
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
     ],
   },

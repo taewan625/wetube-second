6b784d3a57275673c9a3ee99a68905666d60a85c
taewan625 <taewan1997625@gmail.com>
2022-10-06 10:28:34 +0900

    -#17.7

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,4 +1,20 @@
 import multer from "multer";
+import multerS3 from "multer-s3";
+import { S3Client } from "@aws-sdk/client-s3";
+
+// AWS s3에 접근할 수 있는 API key secret 작성
+const s3 = new S3Client({
+  Credential: {
+    accesskeyId: process.env.AWS_ID,
+    secretAccessKey: process.env.AWS_SECRET,
+  },
+});
+
+// aip연결 후 AWS 어느 bucket에 저장할지 설정
+const multerUploader = multerS3({
+  s3: s3,
+  bucket: "setube", // AWS bucket name
+});
 
 export const localsMiddleware = (req, res, next) => {
   //   console.log(req.session);
@@ -32,11 +48,13 @@ export const publicOnlyMiddleware = (req, res, next) => {
 export const avatarUpload = multer({
   dest: "uploads/avatars/",
   limits: { fileSize: 3000000 },
+  storage: multerUploader,
 });
 // edit profile의 input의 파일을 받고 -> router에서 post middleware로 간 후 여기서 uploads에 file 저장 후 postEdit controller로 이동
 export const videoUpload = multer({
   dest: "uploads/videos/",
   limits: { fileSize: 10000000 },
+  storage: multerUploader,
 });
 
 // export const thumbUpload = multer({
